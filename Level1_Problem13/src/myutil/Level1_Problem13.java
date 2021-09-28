47a07a1dc58e304a8036d15fb9b8323a4ac98c3c
Shadesh Ram Gour <86781084+shadeshsaha@users.noreply.github.com>
2021-09-28 09:13:47 +0600

    Add files via upload

---
+++ Level1_Problem13/src/myutil/Level1_Problem13.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myutil;
+
+/**
+ *
+ * @author Shadesh
+ */
+import myutil.*;
+public class Level1_Problem13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Person p = new Person();
+        System.out.println(p.getName());	
+
+    }
+    
+}
